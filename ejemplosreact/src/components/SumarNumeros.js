4aaced88265f888dda1e47c930958e636b5789b7
Sartorius11 <sartoxr@gmail.com>
2022-10-18 14:24:36 +0200

    commit

--- ejemplosreact/src/components/SumarNumeros.js
+++ ejemplosreact/src/components/SumarNumeros.js
@@ -1,17 +1,18 @@
 import gollum from'./../assets/gollum.jpg';
 
 
-function SumarNumeros() {
+function SumarNumeros(props) {
 
 var imageSize={
     with:"150px", height:"150px"
 }
 
-const SumarNumeros = (numero,numero2) => {
-
-  var n1 = numero;
-  var n2 = numero2;
-  var suma = n1+n2;
+const SumarNumeros = (n1,n2) => {
+var n1 = parseInt(props.numero1);
+var n2 = parseInt(props.numero2);
+  // var n1 = numero;
+  // var n2 = numero2;
+   var suma = n1 + n2;
   console.log(suma);
 
 };
@@ -21,7 +22,7 @@ return (
 
     <h1> La suma de numeros </h1>
 
-    <button onClick={() => SumarNumeros(3,8)}>La suma  de 3 + 8 =(mire la consola)</button>{" "}
+    <button onClick={() => SumarNumeros()}>La suma  de 3 + 8 =(mire la consola)</button>{" "}
     <br></br>
     <br></br>
     <img src={gollum}/>
